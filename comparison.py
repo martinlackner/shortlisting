46a4b2a31f8883b4723bc06bffc976988e8ab042
Martin Lackner <unexpected@sent.at>
2020-05-12 11:14:40 +0200

    commit code

--- comparison.py
+++ comparison.py
@@ -1,1 +1,302 @@
-/home/martin/Documents/work/repos/jansvn/code/comparison.py
+import shortlisting as var
+import random
+from preferences import Profile
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+
+def run_single_experiment(varrules, num_voter,
+                          quality, perceived_quality,
+                          num_objective_voters=0):
+
+    num_cand = len(quality)
+
+    bestcands = [c for c in range(len(quality))
+                 if quality[c] == max(quality)]
+
+    averqual = dict.fromkeys(varrules, 0)
+    likelibest = dict.fromkeys(varrules, 0)
+    commeffective = dict.fromkeys(varrules, 0)
+    aversize = dict.fromkeys(varrules, 0)
+    varsize = {rule: [] for rule in varrules}
+
+    # generate votes
+    appr_sets = []
+    for i in range(0, num_voter):
+        appr = []
+        for c in range(num_cand):
+            if i < num_objective_voters:
+                # voter i is objective (uses quality)
+                if random.random() < quality[c]:
+                    appr.append(c)
+            else:
+                # voter i is objective (uses perceived_quality)
+                if random.random() < perceived_quality[c]:
+                    appr.append(c)
+        appr_sets.append(appr)
+    profile = Profile(num_cand)
+    profile.add_preferences(appr_sets)
+
+    # compute rules
+    for rule in varrules:
+        comm = var.compute_rule(rule, profile)
+        if comm:  # comm not empty
+            averqual[rule] += np.average([quality[c] for c in comm])
+        if set(bestcands).issubset(comm):
+            likelibest[rule] += 1
+            if len(comm) <= num_cand / 3:
+                commeffective[rule] += 1
+            # commeffective[rule] += np.average([quality[c] for c in comm])
+        aversize[rule] += len(comm)
+        varsize[rule].append(len(comm))
+
+    return averqual, likelibest, commeffective, aversize, varsize
+
+
+def run_experiments(num_voters, num_cand, xvals, varrules,
+                    averqual_series, likelibest_series,
+                    commeffective_series, aversize_series,
+                    varsize_series, distribution, model):
+    for xval in xvals:
+        for rule in varrules:
+            averqual_series[rule].append(0)
+            likelibest_series[rule].append(0)
+            commeffective_series[rule].append(0)
+            aversize_series[rule].append(0)
+            varsize_series[rule].append([])
+
+        for _ in range(iterations):
+            # quality = [0.9, 0.88, 0.7, 0.65, 0.4, 0.2]
+            if distribution == "uniform":
+                quality = [random.random() for _ in range(num_cand)]
+            elif distribution == "normal":
+                lower, upper = 0, 1
+                mu, sigma = .5, 0.2
+                quality = stats.truncnorm.rvs((lower - mu) / sigma,
+                                              (upper - mu) / sigma,
+                                              loc=mu, scale=sigma,
+                                              size=num_cand)
+            else:
+                raise Exception("Distribution " + distribution + " not known.")
+
+            if model == "noise model":
+                # Compute the noisy version of the candidate quality
+                noisy_quality = [(1 - xval) * quality[c] + xval * 0.5
+                                 for c in range(num_cand)]
+                averqual, likelibest, commeffective, aversize, varsize =\
+                    run_single_experiment(varrules, num_voters,
+                                          quality, noisy_quality)
+            elif model == "bias model":
+                # Compute the biased version of the candidate quality
+                # the (negative) bias applies to half of the cands
+                biased_quality = ([quality[c] * .5
+                                   for c in range(num_cand // 2)]
+                                  + [quality[c]
+                                     for c in range(num_cand // 2, num_cand)])
+                # always bias against objectively best cand.
+                for c in range(num_cand):
+                    if quality[c] == max(quality):
+                        biased_quality[c] = quality[c] * .5
+                averqual, likelibest, commeffective, aversize, varsize =\
+                    run_single_experiment(
+                        varrules, num_voters, quality, biased_quality,
+                        num_objective_voters=int((1 - xval) * num_voters))
+            else:
+                raise Exception("Model " + model + " not known.")
+
+            for rule in varrules:
+                averqual_series[rule][-1] += averqual[rule] * 1. / iterations
+                likelibest_series[rule][-1] += \
+                    likelibest[rule] * 1. / iterations
+                commeffective_series[rule][-1] += \
+                    commeffective[rule] * 1. / iterations
+                aversize_series[rule][-1] += aversize[rule] * 1. / iterations
+                varsize_series[rule][-1].append(varsize[rule])
+
+        for rule in varrules:
+            varsize_series[rule][-1] = np.var(varsize_series[rule][-1])
+
+
+# EXPERIMENTS
+
+distributions = ["normal", "uniform"]
+models = ["noise model", "bias model"]
+
+varrules = ["av", "majority", "firstmajority",
+            "largestgap", "firstkgap5", "sizepriority4"]
+# "next2" ,"2means", "2median"]
+
+num_cand = 30
+num_voters = 100
+
+for model in models:
+    for dist in distributions:
+
+        averqual_series = {rule: [] for rule in varrules}
+        likelibest_series = {rule: [] for rule in varrules}
+        commeffective_series = {rule: [] for rule in varrules}
+        aversize_series = {rule: [] for rule in varrules}
+        varsize_series = {rule: [] for rule in varrules}
+        xvals = np.linspace(0, 1, 21)
+        iterations = 1000
+
+        run_experiments(num_voters, num_cand, xvals, varrules,
+                        averqual_series, likelibest_series,
+                        commeffective_series, aversize_series,
+                        varsize_series, dist, model)
+
+        # PLOTTING
+
+        colors = {
+            "av": "-b",
+            "majority": "-r",
+            "firstmajority": ":g",
+            "largestgap": ":c",
+            "firstkgap5": "--m",
+            "sizepriority4": "-.k",
+            "next2": "--g",
+            "2means": "--r",
+            "2median": "--b",
+        }
+
+        plt.figure(figsize=(12, 12))
+        plt.subplot(4, 1, 1)
+        title = ('Shortlisting experiments - ' + dist
+                 + "ly distributed quality, " + model)
+        # plt.title(title)
+        plt.ylabel('average quality')
+        plt.ylim(0.6, 1.0)
+        for rule in varrules:
+            plt.plot(xvals, averqual_series[rule], colors[rule],
+                     label=var.fullname(rule))
+        # plt.xticks(xvals, [])
+
+        plt.subplot(4, 1, 2)
+        for rule in varrules:
+            plt.plot(xvals, likelibest_series[rule], colors[rule],
+                     label=var.fullname(rule))
+        plt.ylabel('best cand. included')
+        plt.ylim(0.45, 1.01)
+        # plt.xticks(xvals, [])
+
+        plt.subplot(4, 1, 3)
+        for rule in varrules:
+            plt.plot(xvals, aversize_series[rule], colors[rule],
+                     label=var.fullname(rule))
+        plt.ylabel('average size')
+        # plt.xticks(xvals, [])
+
+        plt.subplot(4, 1, 4)
+        for rule in varrules:
+            plt.plot(xvals,
+                     commeffective_series[rule],
+                     colors[rule],
+                     label=var.fullname(rule))
+        plt.ylabel('sensible winner sets')
+        plt.ylim(0, 1.01)
+
+#         plt.subplot(5, 1, 5)
+#         for rule in varrules:
+#             plt.plot(xvals, varsize_series[rule], colors[rule],
+#                      label=var.fullname(rule))
+#         plt.ylabel('variance of size')
+#         # plt.xticks(xvals, [])
+
+        if model == "noise model":
+            plt.xlabel('noise (lambda)')
+        if model == "bias model":
+            plt.xlabel('fraction of biased voters (gamma)')
+
+        plt.legend(loc='upper right')
+        plt.tight_layout()
+        plt.savefig(title.replace(" ", "-"), bbox_inches='tight', pad_inches=0)
+        plt.close()
+
+        ###############################
+
+        plt.figure(figsize=(7, 4))
+        title = ('Shortlisting experiments - average quality -' + dist
+                 + "ly distributed quality, " + model)
+        # plt.title(title)
+        plt.ylabel('average quality')
+        if model == "noise model":
+            plt.ylim(0.5, 0.9)
+        if model == "bias model":
+            plt.ylim(0.5, 0.9)
+        for rule in varrules:
+            plt.plot(xvals, averqual_series[rule], colors[rule],
+                     label=var.fullname(rule))
+
+        if model == "noise model":
+            plt.xlabel('noise (lambda)')
+        if model == "bias model":
+            plt.xlabel('fraction of biased voters (gamma)')
+
+        plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.25),
+                   fancybox=True, shadow=False, ncol=3)
+
+        plt.tight_layout()
+        plt.savefig(title.replace(" ", "-"), pad_inches=0)
+        plt.close()
+
+        plt.figure(figsize=(7, 3.2))
+        title = ('Shortlisting experiments - sensible winner sets -' + dist
+                 + "ly distributed quality, " + model)
+        # plt.title(title)
+        for rule in varrules:
+            plt.plot(xvals,
+                     commeffective_series[rule],
+                     colors[rule],
+                     label=var.fullname(rule))
+        plt.ylabel('sensible winner sets')
+        plt.ylim(0, 1.01)
+
+        if model == "noise model":
+            plt.xlabel('noise (lambda)')
+        if model == "bias model":
+            plt.xlabel('fraction of biased voters (gamma)')
+
+        # plt.legend(loc='best')
+        plt.tight_layout()
+        plt.savefig(title.replace(" ", "-"), pad_inches=0)
+        plt.close()
+
+        plt.figure(figsize=(7, 3.2))
+        title = ('Shortlisting experiments - average size -' + dist
+                 + "ly distributed quality, " + model)
+        # plt.title(title)
+        for rule in varrules:
+            plt.plot(xvals, aversize_series[rule], colors[rule],
+                     label=var.fullname(rule))
+        plt.ylabel('average size')
+
+        if model == "noise model":
+            plt.xlabel('noise (lambda)')
+        if model == "bias model":
+            plt.xlabel('fraction of biased voters (gamma)')
+
+        # plt.legend(loc='best')
+        plt.tight_layout()
+        plt.savefig(title.replace(" ", "-"), pad_inches=0)
+        plt.close()
+
+        plt.figure(figsize=(7, 3.2))
+        title = ('Shortlisting experiments - best cand included -' + dist
+                 + "ly distributed quality, " + model)
+        # plt.title(title)
+        for rule in varrules:
+            plt.plot(xvals, likelibest_series[rule], colors[rule],
+                     label=var.fullname(rule))
+        plt.ylabel('best cand. included')
+        plt.ylim(0.45, 1.01)
+
+        if model == "noise model":
+            plt.xlabel('noise (lambda)')
+        if model == "bias model":
+            plt.xlabel('fraction of biased voters (gamma)')
+
+        # plt.legend(loc='best')
+        plt.tight_layout()
+        plt.savefig(title.replace(" ", "-"), pad_inches=0)
+        plt.close()
--- shortlisting.py
+++ shortlisting.py
@@ -1,1 +1,263 @@
-/home/martin/Documents/work/repos/jansvn/code/shortlisting.py
+from misc import str_candsets
+import numpy as np
+
+
+VARRULES = ["av", "majority", "firstmajority",
+            "largestgap", "firstkgap", "sizepriority",
+            "nextk", "2means", "2median"]
+
+
+def compute_rule(name, profile):
+    """Returns the list of winning committees according to the named rule"""
+    if name == "majority":
+        return compute_majority(profile)
+    elif name == "av":
+        return compute_av(profile)
+    elif name == "firstmajority":
+        return compute_firstmajority(profile)
+    elif name == "largestgap":
+        return compute_largestgap(profile)
+    elif name == "2means":
+        return compute_2means(profile)
+    elif name == "2median":
+        return compute_2median(profile)
+    elif name[:9] == "firstkgap":
+        if len(name) > 9:
+            parameter = int(name[9:])
+        else:
+            parameter = max(len(profile.preferences) / 20, 1)
+        return compute_firstkgap(profile, parameter)
+    elif name[:4] == "next":
+        if name == "nextk" or name == "next":
+            parameter = 2
+        else:
+            parameter = int(name[4:])
+        return compute_nextk(profile, parameter)
+    elif name[:12] == "sizepriority":
+        if len(name) > 12:
+            param = name[12:]
+            size = int(param)
+        else:
+            size = profile.num_cand / 3.
+        return compute_sizepriority(profile, size)
+    else:
+        raise NotImplementedError("Variable multi-winner rule " +
+                                  str(name) + " not known")
+
+
+def fullname(name, parameter=None):
+    """Returns the full name of the voting rule"""
+    if name == "majority":
+        return "0.5|V|-Threshold"
+    elif name == "av":
+        return "Approval Voting"
+    elif name == "firstmajority":
+        return "First Majority"
+    elif name == "largestgap":
+        return "Largest Gap"
+    elif name == "2means":
+        return "2-Means Clustering"
+    elif name == "2median":
+        return "2-Median Clustering"
+    elif name[:9] == "firstkgap":
+        if len(name) > 9:
+            parameter = int(name[9:])
+        if parameter is None:
+            parameter = "?"
+        return "First " + str(parameter) + "-Gap"
+    elif name[:4] == "next":
+        if name == "nextk" or name == "next":
+            parameter = 2
+        else:
+            parameter = int(name[4:])
+        return "Next-" + str(parameter)
+    elif name[:12] == "sizepriority":
+        if len(name) > 12:
+            parameter = "opt=" + name[12:]
+        if parameter is None:
+            return "Size Priority"
+        return "Size Priority"  # ("+str(parameter)+")"
+    else:
+        raise NotImplementedError("Variable multi-winner rule " +
+                                  str(name) + " not known")
+
+
+def allrules(profile, include_resolute=False):
+    """Prints the winning committees for all implemented rules"""
+    for rule in VARRULES.keys():
+        print(VARRULES[rule] + ":")
+        com = compute_rule(rule, profile)
+        print(str_candsets(com))
+
+        if include_resolute:
+            print(VARRULES[rule] + " (with tie-breaking):")
+            com = compute_rule(rule, profile, resolute=True)
+            print(str_candsets(com))
+
+
+########################################################################
+
+
+def approval_scores(profile):
+    appr_score = [0] * profile.num_cand
+    for pref in profile.preferences:
+        for cand in pref.approved:
+            appr_score[cand] += pref.weight
+    return appr_score
+
+
+def compute_av(profile):
+    """Returns the list of winning committees according to Approval Voting"""
+    appr_score = approval_scores(profile)
+    committee = [c for c in range(profile.num_cand)
+                 if appr_score[c] == max(appr_score)]
+    return committee
+
+
+def compute_majority(profile):
+    """Returns the list of winning committees according to the Majority rule"""
+    appr_score = approval_scores(profile)
+    committee = [c for c in range(profile.num_cand)
+                 if appr_score[c] >= profile.totalweight() / 2.]
+    return committee
+
+
+def compute_firstmajority(profile):
+    """Returns the list of winning committees
+    according to the First Majority rule"""
+    appr_score = approval_scores(profile)
+    ordered_cands = sorted(range(profile.num_cand),
+                           key=lambda x: appr_score[x], reverse=True)
+    firstsum = 0
+    committee = []
+    for c in ordered_cands:
+        committee.append(c)
+        firstsum += appr_score[c]
+        if firstsum > sum(appr_score) // 2:
+            break
+
+    return sorted(committee)
+
+
+def compute_largestgap(profile):
+    """Returns the list of winning committees
+    according to the Largest Gap rule"""
+    appr_score = approval_scores(profile)
+    appr_score_sorted = sorted(appr_score, reverse=True)
+    gaps = [appr_score_sorted[i] - appr_score_sorted[i + 1]
+            for i in range(profile.num_cand - 1)]
+    gapvalue = max([appr_score_sorted[i] for i in range(profile.num_cand - 1)
+                    if gaps[i] == max(gaps)])
+    committee = [c for c in range(profile.num_cand)
+                 if appr_score[c] >= gapvalue]
+
+    return committee
+
+
+def compute_firstkgap(profile, paramk):
+    """Returns the list of winning committees
+    according to the First k-Gap rule"""
+    appr_score = approval_scores(profile)
+    appr_score_sorted = sorted(appr_score, reverse=True)
+    kgaps = [i for i in range(profile.num_cand - 1)
+             if appr_score_sorted[i] - appr_score_sorted[i + 1] >= paramk]
+    if not kgaps:
+        committee = list(range(profile.num_cand))
+    else:
+        firstkgap = min(kgaps)
+        threshold = appr_score_sorted[firstkgap]
+        committee = [c for c in range(profile.num_cand)
+                     if appr_score[c] >= threshold]
+    return committee
+
+
+def compute_sizepriority(profile, size):
+    """Returns the list of winning committees
+    according to the Size Priority rule"""
+    appr_score = approval_scores(profile)
+    appr_score_sorted = sorted(appr_score, reverse=True)
+    if isinstance(size, list):
+        priority = size
+    else:
+        # priority = sorted(list(range(profile.num_cand + 1)),
+        #                   key=lambda x: abs(size - x))
+        priority = sorted(list(range(size, profile.num_cand + 1)))
+    for psize in priority:
+        threshold = appr_score_sorted[psize - 1]
+        committee = [c for c in range(profile.num_cand)
+                     if appr_score[c] >= threshold]
+        if len(committee) == psize:
+            return committee
+
+    raise AssertionError("This should not happen. \
+                         Size Priority found no committee.")
+
+
+def compute_nextk(profile, nextk):
+    """Returns the list of winning committees
+    according to the Next-k rule"""
+    appr_score = approval_scores(profile)
+    ordered_cands = sorted(range(profile.num_cand),
+                           key=lambda x: appr_score[x], reverse=True)
+    committee = []
+    for i in range(profile.num_cand):
+        c = ordered_cands[i]
+        committee.append(c)
+        if len(committee) == profile.num_cand:
+            continue
+        # non-tiebreaking
+        if appr_score[c] == appr_score[ordered_cands[i + 1]]:
+            continue
+        score_nextk = sum([appr_score[ordered_cands[j]]
+                           for j in range(i + 1,
+                                          min(i + nextk + 1, profile.num_cand))
+                           ])
+        if score_nextk < appr_score[c]:
+            break
+
+    return sorted(committee)
+
+
+def compute_2means(profile):
+    """Returns the list of winning committees
+    according to the 2-Means Clustering rule"""
+    return mindistance_clustering(profile, "means")
+
+
+def compute_2median(profile):
+    """Returns the list of winning committees
+    according to the 2-Median Clustering rule"""
+    return mindistance_clustering(profile, "median")
+
+
+def mindistance_clustering(profile, objective):
+    def twomeans(cluster1, cluster2):
+        m1 = np.mean(cluster1) if cluster1 else 0
+        m2 = np.mean(cluster2) if cluster2 else 0
+        return (sum([np.abs(x - m1)**2 for x in cluster1]) +
+                sum([np.abs(x - m2)**2 for x in cluster2]))
+
+    def twomedian(cluster1, cluster2):
+        m1 = np.median(cluster1) if cluster1 else 0
+        m2 = np.median(cluster2) if cluster2 else 0
+        return (sum([np.abs(x - m1) for x in cluster1]) +
+                sum([np.abs(x - m2) for x in cluster2]))
+
+    if objective == "means":
+        objectivefct = twomeans
+    elif objective == "median":
+        objectivefct = twomedian
+    else:
+        raise NotImplemented(objective +
+                             " is not an implemented objective function.")
+    appr_score = approval_scores(profile)
+    ordered_cands = sorted(range(profile.num_cand),
+                           key=lambda x: appr_score[x], reverse=True)
+    objfct_scores = []
+    for i in range(profile.num_cand + 1):
+        cluster1 = [appr_score[c] for c in ordered_cands[:i]]
+        cluster2 = [appr_score[c] for c in ordered_cands[i:]]
+        objfct_scores.append(objectivefct(cluster1, cluster2))
+    cut = objfct_scores.index(min(objfct_scores))
+
+    return sorted(ordered_cands[:cut])
